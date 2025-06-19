e39ca309f224e412c4a4f20912b8056dddb560bf
Enochteo <enochowoade@gmail.com>
2025-06-18 20:51:13 -0500

    register auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from .models import db
 from dotenv import load_dotenv
 import os
+from flask_login import LoginManager
+from .models import User
 
 load_dotenv()
 
@@ -22,6 +24,17 @@ def create_app():
     
     with app.app_context():
         db.create_all()
-        
+
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)  
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id)) 
+    
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
     return app
 
